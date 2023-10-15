f7daaef490547015064ff94e991443cbe7dd9ed1
liuyang <1126323596@qq.com>
2023-10-15 12:49:36 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 const ChildComp = forwardRef((props, ref) => {
-  // const sayHello = () => {
-  //   console.log('hello')
-  // }
   useImperativeHandle(ref, () => ({
     sayHello() {
       console.log('hello')
